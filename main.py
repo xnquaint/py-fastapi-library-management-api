4b3b0d52f9639a8d96f9c9c85fc32e5c025fe5bd
Stanislav Vdovychenko <vdovychenkostas@gmail.com>
2025-08-01 00:34:51 +0300

    fix

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def read_author(author_id: int, db: Session = Depends(get_db)) -> models.Author:
 
     return db_author
 
-@app.post("/authors/", response_model=schemas.AuthorCreate)
+@app.post("/authors/", response_model=schemas.AuthorRead)
 def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)) -> models.Author:
     existing = crud.get_author_by_name(db=db, name=author.name)
 
@@ -88,7 +88,7 @@ def update_book(book_id: int, book: schemas.BookCreate, db: Session = Depends(ge
 
     return crud.update_book(db=db, book_id=book_id, book=book)
 
-@app.post("/books/", response_model=schemas.BookCreate)
+@app.post("/books/", response_model=schemas.BookRead)
 def create_book(book: schemas.BookCreate, db: Session = Depends(get_db)) -> models.Book:
     existing_book = crud.get_book_by_name(db=db, name=book.title)
     author = crud.get_author_by_id(db=db, author_id=book.author_id)
